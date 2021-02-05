71a6d9bb701ff34804b9c222dcc3009b790aa461
mk-Guvi <61502453+mk-Guvi@users.noreply.github.com>
2021-02-05 15:49:54 +0530

    author schema

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Created with CodeSandbox
 1.)create config-folder and create db.js file init
 
 2.)add mongoose dependency and reate db connection in db.js and import it in index.js file
+
+3.)create models folder and create the author.js file init.
+
+4.)create the author schema in author.js file.
---
+++ src/models/author.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const AuthorSchema = new Schema({
+  //sql  has inbuilt constraint like we can  mark certain fields as required,but in mongodb cant because we can add any fields,so to overcome this mongoose do constraint check in server itself so that we can save the cleaned data
+  first_name: {
+    type: String, //its JS string constructor
+    required: true
+  },
+  last_name: {
+    type: String,
+    required: true
+  },
+  field_of_Study: {
+    type: String,
+    enum: ["Fiction", "non-Fiction"]
+  },
+
+  date_of_birth: Date,
+  date_of_death: Date
+});
+
+//we should tell the mongoose that it is a part of collection
+const Author = model("Author", AuthorSchema); //Author is name of the collection
+
+module.exports = Author;
